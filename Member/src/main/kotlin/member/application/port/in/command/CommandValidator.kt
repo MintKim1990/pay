4dbd3168321539cd76add1ac5edcaa5c34073a16
MintKim1990 <rlaalsxo1990@gmail.com>
2023-09-18 10:43:00 +0900

    사용자 마이크로 서비스 추가
     - 사용자 마이크로 서비스 모듈 추가 및 의존성 설정
     - 사용자 생성 및 조회 기능 추가

---
+++ Member/src/main/kotlin/member/application/port/in/command/CommandValidator.kt
@@ -0,0 +1,16 @@
+package example.application.port.`in`.command
+
+import javax.validation.ConstraintViolationException
+import javax.validation.Validation
+import javax.validation.Validator
+
+abstract class CommandValidator<T>(
+    private val validator: Validator = Validation.buildDefaultValidatorFactory().validator
+) {
+    fun validate() {
+        val violations = validator.validate(this as T)
+        if (violations.isNotEmpty()) {
+            throw ConstraintViolationException(violations)
+        }
+    }
+}
